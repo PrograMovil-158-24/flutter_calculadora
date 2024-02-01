c84117fb6fa939e88f09a389113135e6f7925411
Ivan Cruz <ivancruzhn@gmail.com>
2024-01-31 20:55:32 -0600

    demo statefulwidget suma de numeros

---
+++ lib/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double primerNumero = 0;
+  double segundoNumero = 0;
+  double resultado = 0.0;
+
+  void sumar() {
+    setState(() {
+      resultado = primerNumero + segundoNumero;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Calculadora'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Número 1',
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  primerNumero = double.parse(value);
+                },
+              ),
+              const Divider(),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Número 2',
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  segundoNumero = double.parse(value);
+                },
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                      onPressed: sumar, child: const Icon(Icons.add)),
+                  const SizedBox(width: 10),
+                  FloatingActionButton(
+                      onPressed: sumar, child: const Icon(Icons.minimize)),
+
+                  // multiplicar
+                  // dividir
+                ],
+              ),
+              const Divider(),
+              Text("Resultado: $resultado"),
+            ])));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculadora/home.dart';
+
+void main()=> runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true)
+    );
+  }
+}
